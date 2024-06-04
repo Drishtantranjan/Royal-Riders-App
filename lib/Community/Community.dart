4e9d7befcefbbebe1ff7afeae2aa43c32c23647a
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-04 22:38:10 +0530

    Ride screen

---
+++ lib/Community/Community.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../Common_widgets/Tab_Index_Line.dart';
+import 'MyRideScreen.dart';
+
+class CommunityScreen extends StatefulWidget {
+  const CommunityScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CommunityScreen> createState() => _CommunityScreenState();
+}
+
+class _CommunityScreenState extends State<CommunityScreen> {
+  bool isMyZoneSelected = true; // State variable for "My Zone" tab
+
+  void _onTabTapped(bool isMyZone) {
+    setState(() {
+      isMyZoneSelected = isMyZone;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          color: Colors.black,
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                GestureDetector(
+                  onTap: () => _onTabTapped(true),
+                  child: Column(
+                    children: [
+                      Text(
+                        "My Zone",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: isMyZoneSelected ? Colors.white : Colors.grey,
+                          fontWeight: isMyZoneSelected
+                              ? FontWeight.bold
+                              : FontWeight.normal,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TabIndexLine(isSelected: isMyZoneSelected),
+                    ],
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () => _onTabTapped(false),
+                  child: Column(
+                    children: [
+                      Text(
+                        "My Rides",
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: isMyZoneSelected ? Colors.grey : Colors.white,
+                          fontWeight: isMyZoneSelected
+                              ? FontWeight.normal
+                              : FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      TabIndexLine(isSelected: !isMyZoneSelected),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(
+          child: isMyZoneSelected ? MyZone() : MyRides(),
+        ),
+      ],
+    );
+  }
+}
+
+// Define two different widgets for the two screens
+class MyZone extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const ZoneScreen();
+  }
+}
+
+class MyRides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyRidesScreen();
+  }
+}
---
+++ lib/Community/MyRideScreen.dart
@@ -0,0 +1,81 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../Common_widgets/Button.dart';
+import '../Common_widgets/RideContainer.dart'; // Import Firestore
+
+class MyRidesScreen extends StatelessWidget {
+  MyRidesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection('Assets')
+                .doc('Carousel_Images')
+                .snapshots(),
+            builder: (BuildContext context,
+                AsyncSnapshot<DocumentSnapshot> snapshot) {
+              if (snapshot.hasError) {
+                return Text(
+                  'Error: ${snapshot.error}',
+                  style: const TextStyle(color: Colors.white),
+                );
+              }
+              print(
+                'Error: ${snapshot.error}',
+              );
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              }
+
+              List<String> imgList =
+                  List<String>.from(snapshot.data!.get('Images'));
+              return CarouselSlider(
+                options: CarouselOptions(
+                  autoPlay: true,
+                  aspectRatio: 2.0,
+                  enlargeCenterPage: true,
+                  viewportFraction: 1.0,
+                ),
+                items: imgList
+                    .map(
+                      (item) => SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Image.network(
+                            item,
+                            fit: BoxFit.cover,
+                            width: MediaQuery.of(context).size.width,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
+              );
+            },
+          ),
+          const Padding(
+            padding: EdgeInsets.all(15.0),
+            child: Text(
+              "Join Rides",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
+          ),
+          const RideContainer(),
+          const SizedBox(height: 10),
+          RoundedButton(title: "Create Rides", onPressed: () {}),
+        ],
+      ),
+    );
+  }
+}
