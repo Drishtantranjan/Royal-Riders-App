e4bcb962bcc9aa1c22a7d33030b36664276a9592
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-07 22:49:42 +0530

    state management Implementation in sign In screen

--- lib/Login&SignUp/Presentation/view/Sign_1.dart
+++ lib/Login&SignUp/Presentation/view/Sign_1.dart
@@ -1,10 +1,12 @@
-import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:royal_riders_application/Common_widgets/Button.dart';
+import 'package:royal_riders_application/Home/presentation/view/HomePage.dart';
+import 'package:royal_riders_application/Login&SignUp/Cubit/auth_cubit.dart';
+import 'package:royal_riders_application/Login&SignUp/Cubit/auth_state.dart';
 import 'OtpFieldwithLabel.dart';
-import 'SignUp_2.dart';
 import '../../../../../Common_widgets/TextFieldwithLable.dart';
 
 class Sign1 extends StatefulWidget {
@@ -18,11 +20,9 @@ class _Sign1State extends State<Sign1> {
   bool _isLoading = false;
   bool _phoneNumberFilled = false;
   bool _phoneNumberError = false;
+  bool _showOTPField = false;
   String _phoneNumberErrorMessage = '';
   int _buttonPressed = 0;
-  int _resendOTPTimerDuration = 30;
-  String _verificationId = '';
-  final FirebaseAuth _auth = FirebaseAuth.instance;
   final TextEditingController _phoneController =
       TextEditingController(text: "+91");
   final TextEditingController _otpController = TextEditingController();
@@ -34,115 +34,6 @@ class _Sign1State extends State<Sign1> {
     _otpController.dispose();
     _otpFocusNode.dispose();
     super.dispose();
-  }
-
-  Future<void> _verifyPhoneNumber() async {
-    setState(() {
-      _resendOTPTimerDuration = 30;
-    });
-    print('ph number ${_phoneController.text}');
-    await _auth.verifyPhoneNumber(
-      phoneNumber: _phoneController.text,
-      verificationCompleted: (PhoneAuthCredential credential) async {
-        await _auth.signInWithCredential(credential);
-      },
-      verificationFailed: (FirebaseAuthException e) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-              content: Text('Failed to verify phone number: ${e.message}')),
-        );
-      },
-      codeSent: (String verificationId, int? resendToken) {
-        setState(() {
-          _verificationId = verificationId;
-        });
-      },
-      codeAutoRetrievalTimeout: (String verificationId) {
-        setState(() {
-          _verificationId = verificationId;
-        });
-      },
-    );
-  }
-
-  void _handleVerifyButtonPressed() {
-    RegExp phoneNumberRegExp = RegExp(r'^\+[0-9]{12}$');
-    if (_phoneController.text.isEmpty) {
-      setState(() {
-        _phoneNumberError = true;
-        _phoneNumberErrorMessage = 'Field cannot be empty';
-      });
-    } else if (phoneNumberRegExp.hasMatch(_phoneController.text)) {
-      if (_buttonPressed == 0) {
-        _verifyPhoneNumber();
-        setState(() {
-          _buttonPressed = 1;
-        });
-      } else {
-        _signInWithOTP();
-      }
-    } else {
-      setState(() {
-        _phoneNumberError = true;
-        _phoneNumberErrorMessage = 'Invalid phone number';
-      });
-    }
-  }
-
-  Future<void> _signInWithOTP() async {
-    try {
-      setState(() {
-        _isLoading = true;
-      });
-
-      PhoneAuthCredential credential = PhoneAuthProvider.credential(
-        verificationId: _verificationId,
-        smsCode: _otpController.text,
-      );
-      print('SMS Code: ${_otpController.text}');
-
-      await _auth.signInWithCredential(credential);
-
-      // Simulate a delay for the loader
-      await Future.delayed(const Duration(seconds: 4));
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const Sign2()),
-      );
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Failed to sign in: $e')),
-      );
-    } finally {
-      setState(() {
-        _isLoading = false;
-      });
-    }
-  }
-
-  void _showLoader() {
-    setState(() {
-      _isLoading = true;
-    });
-
-    OverlayEntry loader = OverlayEntry(
-      builder: (context) => Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          child: Image.asset('assets/SignUp/loader.gif'),
-        ),
-      ),
-    );
-
-    Overlay.of(context)?.insert(loader);
-
-    Future.delayed(Duration(seconds: 2), () {
-      loader.remove();
-      setState(() {
-        _isLoading = false;
-      });
-    });
   }
 
   @override
@@ -199,24 +90,60 @@ class _Sign1State extends State<Sign1> {
                     borderSide: BorderSide(color: Colors.red),
                   ),
                 ),
-                if (_verificationId.isNotEmpty)
-                  Column(
-                    children: [
-                      const SizedBox(height: 20),
-                      Otpfieldwithlabel(
-                        label: "Enter OTP",
-                        controller: _otpController,
-                      ),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
                 const SizedBox(height: 20),
-                RoundedButton(
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  title: _buttonPressed == 0 ? "Send OTP" : "Verify Number",
-                  onPressed: () {
-                    _handleVerifyButtonPressed();
-                    _showLoader();
+                BlocConsumer<AuthCubit, AuthState>(
+                  listener: (context, state) {
+                    if (state is AuthCodeSentState) {
+                      setState(() {
+                        _buttonPressed = 1;
+                        _showOTPField = true;
+                      });
+                    } else if (state is AuthLoggedInState) {
+                      Navigator.pushReplacement(
+                        context,
+                        CupertinoPageRoute(builder: (context) => HomePage()),
+                      );
+                    }
+                  },
+                  builder: (context, state) {
+                    if (state is AuthLoadingState) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: Colors.white,
+                        ),
+                      );
+                    }
+
+                    return Column(
+                      children: [
+                        if (_showOTPField) ...[
+                          Otpfieldwithlabel(
+                            label: "Enter OTP",
+                            controller: _otpController,
+                          ),
+                          const SizedBox(height: 20),
+                          RoundedButton(
+                            padding: const EdgeInsets.symmetric(horizontal: 0),
+                            title: "Verify Number",
+                            onPressed: () {
+                              String otp = _otpController.text;
+                              BlocProvider.of<AuthCubit>(context)
+                                  .verifyOTP(otp);
+                            },
+                          ),
+                        ] else ...[
+                          RoundedButton(
+                            padding: const EdgeInsets.symmetric(horizontal: 0),
+                            title: "Send OTP",
+                            onPressed: () {
+                              String phoneNumber = _phoneController.text;
+                              BlocProvider.of<AuthCubit>(context)
+                                  .sentOTP(phoneNumber);
+                            },
+                          ),
+                        ],
+                      ],
+                    );
                   },
                 ),
               ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:royal_riders_application/Login&SignUp/Cubit/auth_cubit.dart';
 import 'package:royal_riders_application/Login&SignUp/Presentation/view/SignUp_Screen.dart';
 import 'package:royal_riders_application/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  Bloc.observer = AppBlocObserver();
+
   runApp(const MyApp());
 }
 
@@ -15,39 +16,15 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SignUp(),
-    );
-  }
-}
-
-class AppBlocObserver extends BlocObserver {
-  AppBlocObserver();
-
-  final String _logTag = 'AppBlocObserver';
-
-  @override
-  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
-    super.onChange(bloc, change);
-    // if (bloc is AuthBloc && change.nextState is AuthUnauthenticated) {
-    //   AppConstants.navigatorKey.currentState!.pushNamedAndRemoveUntil(
-    //       Routes.signin, (Route<dynamic> route) => false);
-    // }
-    print(
-      '$_logTag.onChange ${bloc.runtimeType} - $change',
-    );
-  }
-
-  @override
-  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
-    print(
-      '$_logTag.onError ${bloc.runtimeType} - $error',
-    );
-    super.onError(bloc, error, stackTrace);
+    return BlocProvider(
+        create: (context) => AuthCubit(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: SignUp(),
+        ));
   }
 }
