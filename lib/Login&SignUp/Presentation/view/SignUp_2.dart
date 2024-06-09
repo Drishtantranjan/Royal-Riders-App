73e57d299cd71fd723812977c5b70613fe616066
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-09 08:45:16 +0530

    SignUp_2 started

--- lib/Login&SignUp/Presentation/view/SignUp_2.dart
+++ lib/Login&SignUp/Presentation/view/SignUp_2.dart
@@ -34,108 +34,102 @@ class _Sign2State extends State<Sign2> {
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          stops: [0.65, 0.98],
-          colors: [Colors.black12, Color(0XFF737373)],
+        image: DecorationImage(
+          image: AssetImage('assets/SignUp/sign2.png'),
+          fit: BoxFit.cover,
         ),
       ),
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/SignUp/Group17.png'),
-              fit: BoxFit.fitHeight),
-        ),
-        child: Scaffold(
-          backgroundColor: Colors.transparent,
-          body: SingleChildScrollView(
-            padding: const EdgeInsets.only(left: 20.0, right: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 50),
-                Row(
-                  children: [
-                    Container(
-                      height: 5,
-                      width: MediaQuery.of(context).size.width / 2 - 25,
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          bottomLeft: Radius.circular(20),
-                        ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.only(left: 20.0, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 50),
+              Row(
+                children: [
+                  Container(
+                    height: 5,
+                    width: MediaQuery.of(context).size.width / 2 - 25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        bottomLeft: Radius.circular(20),
                       ),
                     ),
-                    Container(
-                      height: 5,
-                      width: 5,
-                      color: Colors.transparent,
+                  ),
+                  Container(
+                    height: 5,
+                    width: 5,
+                    color: Colors.transparent,
+                  ),
+                  Container(
+                    height: 5,
+                    width: MediaQuery.of(context).size.width / 2 - 25,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        bottomRight: Radius.circular(20),
+                      ),
                     ),
-                    Container(
-                      height: 5,
-                      width: MediaQuery.of(context).size.width / 2 - 25,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(20),
-                              bottomRight: Radius.circular(20))),
-                    ),
-                  ],
+                  ),
+                ],
+              ),
+              const SizedBox(height: 25),
+              const Text(
+                'Lets know your Vehicle',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
                 ),
-                const SizedBox(height: 25),
-                const Text(
-                  'Lets know your Vehicle',
-                  style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white),
-                ),
-                const SizedBox(height: 3),
-                const SizedBox(height: 15),
-                TextFieldWithLabel(
-                  label: 'Owner\'s Name',
-                  controller: nameController,
-                ),
-                const SizedBox(height: 20),
-                TextFieldWithLabel(
-                  label: 'Vehicle registration Number',
-                  controller: vRegistrationController,
-                ),
-                const SizedBox(height: 20),
-                TextFieldWithLabel(
-                  label: 'Vehicle Model Number',
-                  controller: vModelController,
-                ),
-                const SizedBox(height: 20),
-                TextFieldWithLabel(
-                  label: 'Vehicle Color',
-                  controller: vColorController,
-                ),
-                const SizedBox(height: 20),
-                TextFieldWithLabel(
-                  label: 'Vehicle Nickname (optional)',
-                  controller: vNameController,
-                ),
-                const SizedBox(height: 20),
-                TextFieldWithLabel(
-                  label: 'Vehicle Age',
-                  controller: vAgeController,
-                ),
-                const SizedBox(height: 20),
-                RoundedButton(
-                  title: "Create account",
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const HomePage()),
-                    );
-                  },
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 3),
+              const SizedBox(height: 15),
+              TextFieldWithLabel(
+                label: 'Owner\'s Name',
+                controller: nameController,
+              ),
+              const SizedBox(height: 20),
+              TextFieldWithLabel(
+                label: 'Vehicle registration Number',
+                controller: vRegistrationController,
+              ),
+              const SizedBox(height: 20),
+              TextFieldWithLabel(
+                label: 'Vehicle Model Number',
+                controller: vModelController,
+              ),
+              const SizedBox(height: 20),
+              TextFieldWithLabel(
+                label: 'Vehicle Color',
+                controller: vColorController,
+              ),
+              const SizedBox(height: 20),
+              TextFieldWithLabel(
+                label: 'Vehicle Nickname (optional)',
+                controller: vNameController,
+              ),
+              const SizedBox(height: 20),
+              TextFieldWithLabel(
+                label: 'Vehicle Age',
+                controller: vAgeController,
+              ),
+              const SizedBox(height: 20),
+              RoundedButton(
+                title: "Create account",
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => const HomePage()),
+                  );
+                },
+                padding: const EdgeInsets.symmetric(horizontal: 0),
+              ),
+            ],
           ),
         ),
       ),
