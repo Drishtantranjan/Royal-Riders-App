95e6a33e8dc55e923445fcec58f8f2cbbdaee553
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-11 17:00:50 +0530

    Sign2 Message

--- lib/Login&SignUp/Sign_1 Cubit/auth_cubit.dart
+++ lib/Login&SignUp/Sign_1 Cubit/auth_cubit.dart
@@ -1,9 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:royal_riders_application/Login&SignUp/Sign_1%20Cubit/auth_state.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseMessaging _messaging = FirebaseMessaging.instance;
 
   AuthCubit() : super(AuthInitialState());
 
@@ -41,10 +45,42 @@ class AuthCubit extends Cubit<AuthState> {
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
       if (userCredential.user != null) {
+        sendPushNotification();
         emit(AuthLoggedInState(userCredential.user!));
       }
     } on FirebaseAuthException catch (ex) {
       emit(AuthErrorState(ex.message.toString()));
+    }
+  }
+
+  Future<void> sendPushNotification() async {
+    try {
+      final String serverToken = 'YOUR_SERVER_KEY_HERE';
+      final response = await http.post(
+        Uri.parse('https://fcm.googleapis.com/fcm/send'),
+        headers: <String, String>{
+          'Content-Type': 'application/json',
+          'Authorization': 'key=$serverToken',
+        },
+        body: jsonEncode(
+          <String, dynamic>{
+            'notification': <String, dynamic>{
+              'body': 'Account created successfully!',
+              'title': 'Account Creation'
+            },
+            'priority': 'high',
+            'to': await _messaging.getToken(),
+          },
+        ),
+      );
+
+      if (response.statusCode == 200) {
+        print("Notification sent successfully");
+      } else {
+        print("Notification failed to send");
+      }
+    } catch (e) {
+      print("Error sending notification: $e");
     }
   }
 }
