7fed9fc4f07e6bf0b0f8080d7c822ea92e76d2c4
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-24 03:30:45 +0530

    local db updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,20 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:royal_riders_application/Login&SignUp/Presentation/view/SignUp_2.dart';
 import 'package:royal_riders_application/Login&SignUp/Sign_1%20Cubit/auth_cubit.dart';
 import 'package:royal_riders_application/Login&SignUp/Presentation/view/SignUp_Screen.dart';
+import 'package:royal_riders_application/Login&SignUp/data/hive_model/vehicle_model.dart';
 import 'package:royal_riders_application/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Hive.initFlutter();
+  Hive.registerAdapter(VehicleAdapter());
+  await Hive.openBox<Vehicle>('vehicleBox');
 
   AwesomeNotifications().initialize(
       null,
@@ -34,7 +41,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primarySwatch: Colors.blue,
           ),
-          home: const SignUp(),
+          home: const Sign2(),
         ));
   }
 }
