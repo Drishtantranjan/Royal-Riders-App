cb0897c8cb70919462274fa5e9b6621f4721ed94
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-24 03:31:09 +0530

    local db updated

---
+++ lib/Login&SignUp/data/hive_model/vehicle_model.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vehicle_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class VehicleAdapter extends TypeAdapter<Vehicle> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Vehicle read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Vehicle(
+      name: fields[0] as String,
+      registrationNumber: fields[1] as String,
+      modelNumber: fields[2] as String,
+      color: fields[3] as String,
+      nickname: fields[4] as String,
+      age: fields[5] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Vehicle obj) {
+    writer
+      ..writeByte(6)
+      ..writeByte(0)
+      ..write(obj.name)
+      ..writeByte(1)
+      ..write(obj.registrationNumber)
+      ..writeByte(2)
+      ..write(obj.modelNumber)
+      ..writeByte(3)
+      ..write(obj.color)
+      ..writeByte(4)
+      ..write(obj.nickname)
+      ..writeByte(5)
+      ..write(obj.age);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VehicleAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
