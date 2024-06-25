f56c1461c1d2988d2113252bdcb3dd479e645f57
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-25 12:44:10 +0530

    cubit addded

--- lib/Login&SignUp/Presentation/view/SignUp_2.dart
+++ lib/Login&SignUp/Presentation/view/SignUp_2.dart
@@ -1,59 +1,17 @@
-import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:hive/hive.dart';
 import 'package:royal_riders_application/Login&SignUp/Sign_1%20Cubit/auth_cubit.dart';
 import 'package:royal_riders_application/Login&SignUp/Sign_1%20Cubit/auth_state.dart';
+import 'package:royal_riders_application/Login&SignUp/Sign_2%20Cubit/sign2_cubit.dart';
+import 'package:royal_riders_application/Login&SignUp/data/hive_model/vehicle_model.dart';
 import 'package:royal_riders_application/home/presentation/view/HomePage.dart';
 import '../../../../../Common_widgets/TextFieldwithLable.dart';
 import '../../../Common_widgets/Button.dart';
 
-class Sign2 extends StatefulWidget {
+class Sign2 extends StatelessWidget {
   const Sign2({Key? key}) : super(key: key);
-
-  @override
-  _Sign2State createState() => _Sign2State();
-}
-
-class _Sign2State extends State<Sign2> {
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController vRegistrationController = TextEditingController();
-  final TextEditingController vModelController = TextEditingController();
-  final TextEditingController vColorController = TextEditingController();
-  final TextEditingController vNameController = TextEditingController();
-  final TextEditingController vAgeController = TextEditingController();
-
-  @override
-  void dispose() {
-    nameController.dispose();
-    vRegistrationController.dispose();
-    vModelController.dispose();
-    vColorController.dispose();
-    vNameController.dispose();
-    vAgeController.dispose();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    AwesomeNotifications().isNotificationAllowed().then(
-      (isAllowed) {
-        if (!isAllowed) {
-          AwesomeNotifications().requestPermissionToSendNotifications();
-        }
-      },
-    );
-    super.initState();
-  }
-
-  triggerNotification() {
-    AwesomeNotifications().createNotification(
-        content: NotificationContent(
-            id: 10,
-            channelKey: 'basic_channel',
-            title: 'Royal Riders',
-            body:
-                'Welcome to Royal Riders your account has been created sucessfully, gear up for some thrill. Happy to have you on board 😁'));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -68,115 +26,124 @@ class _Sign2State extends State<Sign2> {
             );
           }
         },
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/SignUp/sign2.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Scaffold(
-            backgroundColor: Colors.transparent,
-            body: SingleChildScrollView(
-              padding: const EdgeInsets.only(left: 20, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(height: 50),
-                  Row(
-                    children: [
-                      Container(
-                        height: 5,
-                        width: MediaQuery.of(context).size.width / 2 - 25,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(20),
-                            bottomLeft: Radius.circular(20),
-                          ),
+        child: BlocProvider(
+          create: (context) => Sign2Cubit(),
+          child: BlocBuilder<Sign2Cubit, void>(
+            builder: (context, state) {
+              final sign2Cubit = context.read<Sign2Cubit>();
+
+              return Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/SignUp/sign2.png'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                child: Scaffold(
+                  backgroundColor: Colors.transparent,
+                  body: SingleChildScrollView(
+                    padding: const EdgeInsets.only(left: 20, right: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 50),
+                        Row(
+                          children: [
+                            Container(
+                              height: 5,
+                              width: MediaQuery.of(context).size.width / 2 - 25,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  bottomLeft: Radius.circular(20),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              height: 5,
+                              width: 5,
+                              color: Colors.transparent,
+                            ),
+                            Container(
+                              height: 5,
+                              width: MediaQuery.of(context).size.width / 2 - 25,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.only(
+                                  topRight: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                      Container(
-                        height: 5,
-                        width: 5,
-                        color: Colors.transparent,
-                      ),
-                      Container(
-                        height: 5,
-                        width: MediaQuery.of(context).size.width / 2 - 25,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(20),
-                            bottomRight: Radius.circular(20),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 25),
-                  const Text(
-                    'Lets know your Vehicle',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(height: 3),
-                  const SizedBox(height: 15),
-                  TextFieldWithLabel(
-                    label: 'Owner\'s Name',
-                    controller: nameController,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFieldWithLabel(
-                    label: 'Vehicle registration Number',
-                    controller: vRegistrationController,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFieldWithLabel(
-                    label: 'Vehicle Model Number',
-                    controller: vModelController,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFieldWithLabel(
-                    label: 'Vehicle Color',
-                    controller: vColorController,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFieldWithLabel(
-                    label: 'Vehicle Nickname (optional)',
-                    controller: vNameController,
-                  ),
-                  const SizedBox(height: 20),
-                  TextFieldWithLabel(
-                    label: 'Vehicle Age',
-                    controller: vAgeController,
-                  ),
-                  const SizedBox(height: 20),
-                  BlocBuilder<AuthCubit, AuthState>(
-                    builder: (context, state) {
-                      if (state is AuthLoadingState) {
-                        return Center(
-                          child: CircularProgressIndicator(
+                        const SizedBox(height: 25),
+                        const Text(
+                          'Lets know your Vehicle',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
                             color: Colors.white,
                           ),
-                        );
-                      }
-                      return RoundedButton(
-                        title: "Create account",
-                        onPressed: () {
-                          triggerNotification();
-                          // Add the logic to create an account here?context.read<AuthCubit>().sendPushNotification();
-                        },
-                        padding: const EdgeInsets.symmetric(horizontal: 0),
-                      );
-                    },
+                        ),
+                        const SizedBox(height: 3),
+                        const SizedBox(height: 15),
+                        TextFieldWithLabel(
+                          label: 'Owner\'s Name',
+                          controller: sign2Cubit.nameController,
+                        ),
+                        const SizedBox(height: 20),
+                        TextFieldWithLabel(
+                          label: 'Vehicle registration Number',
+                          controller: sign2Cubit.vRegistrationController,
+                        ),
+                        const SizedBox(height: 20),
+                        TextFieldWithLabel(
+                          label: 'Vehicle Model Number',
+                          controller: sign2Cubit.vModelController,
+                        ),
+                        const SizedBox(height: 20),
+                        TextFieldWithLabel(
+                          label: 'Vehicle Color',
+                          controller: sign2Cubit.vColorController,
+                        ),
+                        const SizedBox(height: 20),
+                        TextFieldWithLabel(
+                          label: 'Vehicle Nickname (optional)',
+                          controller: sign2Cubit.vNameController,
+                        ),
+                        const SizedBox(height: 20),
+                        TextFieldWithLabel(
+                          label: 'Vehicle Age',
+                          controller: sign2Cubit.vAgeController,
+                        ),
+                        const SizedBox(height: 20),
+                        BlocBuilder<AuthCubit, AuthState>(
+                          builder: (context, state) {
+                            if (state is AuthLoadingState) {
+                              return Center(
+                                child: CircularProgressIndicator(
+                                  color: Colors.white,
+                                ),
+                              );
+                            }
+                            return RoundedButton(
+                              title: "Create account",
+                              onPressed: () {
+                                sign2Cubit.createAccount();
+                              },
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 0),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ],
-              ),
-            ),
+                ),
+              );
+            },
           ),
         ),
       ),
