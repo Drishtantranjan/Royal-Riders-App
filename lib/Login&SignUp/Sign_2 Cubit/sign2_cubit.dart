4a0b576fd5d93450b1321ef09d7c70aafb9b8720
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-25 12:44:31 +0530

    cubit addded

---
+++ lib/Login&SignUp/Sign_2 Cubit/sign2_cubit.dart
@@ -0,0 +1,63 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:royal_riders_application/Login&SignUp/data/hive_model/vehicle_model.dart';
+
+class Sign2Cubit extends Cubit<void> {
+  Sign2Cubit() : super(null);
+
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController vRegistrationController = TextEditingController();
+  final TextEditingController vModelController = TextEditingController();
+  final TextEditingController vColorController = TextEditingController();
+  final TextEditingController vNameController = TextEditingController();
+  final TextEditingController vAgeController = TextEditingController();
+
+  @override
+  Future<void> close() {
+    nameController.dispose();
+    vRegistrationController.dispose();
+    vModelController.dispose();
+    vColorController.dispose();
+    vNameController.dispose();
+    vAgeController.dispose();
+    return super.close();
+  }
+
+  void triggerNotification() {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 10,
+        channelKey: 'basic_channel',
+        title: 'Royal Riders',
+        body:
+            'Welcome to Royal Riders your account has been created successfully, gear up for some thrill. Happy to have you on board 😁',
+      ),
+    );
+  }
+
+  Future<void> createAccount() async {
+    final box = Hive.box<Vehicle>('vehicleBox');
+    final vehicle = Vehicle(
+      name: nameController.text,
+      registrationNumber: vRegistrationController.text,
+      modelNumber: vModelController.text,
+      color: vColorController.text,
+      nickname: vNameController.text,
+      age: vAgeController.text,
+    );
+    await box.add(vehicle);
+
+    // Print vehicle details to console
+    print("Vehicle Details:");
+    print("Name: ${vehicle.name}");
+    print("Registration Number: ${vehicle.registrationNumber}");
+    print("Model Number: ${vehicle.modelNumber}");
+    print("Color: ${vehicle.color}");
+    print("Nickname: ${vehicle.nickname}");
+    print("Age: ${vehicle.age}");
+
+    triggerNotification();
+  }
+}
