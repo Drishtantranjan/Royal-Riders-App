e50d2c5a54ff8f59f7b076d85ad922ba9a5eb41d
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-06-28 16:54:47 +0530

    splash screen added

---
+++ lib/Splash/Splash_Screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:royal_riders_application/Login&SignUp/Presentation/view/SignUp_2.dart';
+import 'package:royal_riders_application/Login&SignUp/Presentation/view/SignUp_Screen.dart';
+// Replace with your actual home page
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2), () {
+      // Navigator.pushReplacement(
+      //   context,
+      //   MaterialPageRoute(
+      //       builder: (context) =>
+      //           const SignUp()), // Replace with your actual home page
+      // );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.red,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Royal',
+              style: TextStyle(
+                fontFamily: 'GeorgeDemo',
+                fontSize: 60,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Riders',
+              style: TextStyle(
+                fontFamily: 'GeorgeDemo',
+                fontSize: 70,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2.5,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
