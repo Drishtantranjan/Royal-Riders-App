5af7fdadbb8daa83212bf788b0b3efd93140e565
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-07-02 23:59:45 +0530

    vehicle model

--- lib/Login&SignUp/data/hive_model/vehicle_model.g.dart
+++ lib/Login&SignUp/data/hive_model/vehicle_model.g.dart
@@ -19,17 +19,18 @@ class VehicleAdapter extends TypeAdapter<Vehicle> {
     return Vehicle(
       name: fields[0] as String,
       registrationNumber: fields[1] as String,
+      modelName: fields[3] as String,
       modelNumber: fields[2] as String,
-      color: fields[3] as String,
-      nickname: fields[4] as String,
-      age: fields[5] as String,
+      color: fields[4] as String,
+      nickname: fields[5] as String,
+      age: fields[6] as String,
     );
   }
 
   @override
   void write(BinaryWriter writer, Vehicle obj) {
     writer
-      ..writeByte(6)
+      ..writeByte(7)
       ..writeByte(0)
       ..write(obj.name)
       ..writeByte(1)
@@ -37,10 +38,12 @@ class VehicleAdapter extends TypeAdapter<Vehicle> {
       ..writeByte(2)
       ..write(obj.modelNumber)
       ..writeByte(3)
-      ..write(obj.color)
+      ..write(obj.modelName)
       ..writeByte(4)
-      ..write(obj.nickname)
+      ..write(obj.color)
       ..writeByte(5)
+      ..write(obj.nickname)
+      ..writeByte(6)
       ..write(obj.age);
   }
 
