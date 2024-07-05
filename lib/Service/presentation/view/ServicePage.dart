3d13564993f05341181584c3c0ceab9b48f64c4c
Drishtantranjan <drishtantranjan11122000@gmail.com>
2024-07-05 08:48:36 +0530

    service page

--- lib/Service/presentation/view/ServicePage.dart
+++ lib/Service/presentation/view/ServicePage.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:royal_riders_application/Common_widgets/Button.dart';
 import 'package:royal_riders_application/Common_widgets/appBar_with_divider.dart';
+import 'package:royal_riders_application/Service/presentation/view/MapboxMapPage.dart';
 
 class Servicepage extends StatelessWidget {
   const Servicepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AppbarWithDivider(title: "Service Mode"),
-        RoundedButton(title: "Locate Nearest Service Center", onPressed: () {})
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          AppbarWithDivider(title: "Service Mode"),
+          RoundedButton(
+            title: "Locate Nearest Service Center",
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => MapboxMapPage()),
+              );
+            },
+          ),
+        ],
+      ),
     );
   }
 }
